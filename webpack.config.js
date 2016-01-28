5b90b9f2d6edeecc3e74e98884b548bae1bd961c
Grgur Grisogono <grgur@grgur.com>
2016-01-28 14:25:09 -0500

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,51 @@
+const webpack = require('webpack');
+const path = require('path');
+
+module.exports = {
+  devtool: process.env.NODE_ENV === 'production' ?  'cheap-module-source-map' : 'eval',
+  context: path.join(__dirname, './client'),
+  entry: {
+    jsx: './index.js',
+    html: './index.html',
+    vendor: ['react']
+  },
+  output: {
+    path: path.join(__dirname, './static'),
+    filename: 'bundle.js',
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.html$/,
+        loader: 'file?name=[name].[ext]'
+      },
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        loaders: [
+          'babel-loader'
+        ]
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
+  plugins: [
+    new webpack.optimize.CommonsChunkPlugin('vendor', 'vendor.bundle.js'),
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: false
+      },
+      sourceMap: false
+    }),
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'development')
+      }
+    })
+  ],
+  devServer: {
+    contentBase: './client'
+  }
+};
